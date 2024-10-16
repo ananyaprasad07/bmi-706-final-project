1861a916e9c522ff964e148ca68ba7a221d6e2ac
ananyaprasad07 <ananya_prasad@hms.harvard.edu>
2024-10-15 23:07:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,7 +23,7 @@ subset = neiss_products[neiss_products["Product_1"].isin(products)]
 chart = alt.Chart(subset).mark_bar().encode(
     x=alt.X("Product_1:O", title="Product Type"),
     y=alt.Y("count():Q", title="Number of Injuries"),
-    color=alt.Color('Disposition:O', title='Disposition', scale=alt.Scale(scheme='viridis'), legend=alt.Legend(title="Disposition")),
+    color=alt.Color('Disposition:O', title='Disposition', scale=alt.Scale(scheme='reds'), legend=alt.Legend(title="Disposition")),
     tooltip=["Disposition", "Product_1"]
 ).properties(
     title='Head Injury Severity by Product',
