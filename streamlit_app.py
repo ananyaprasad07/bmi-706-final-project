62a7091d6ea25a14cb57093fdbf10d26f9f00246
ananyaprasad07 <ananya_prasad@hms.harvard.edu>
2024-10-15 23:43:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,13 +14,15 @@ neiss_products = neiss
 top_products = neiss_products['Product_1'].value_counts().nlargest(20).index
 neiss_products = neiss_products[neiss_products['Product_1'].isin(top_products)]
 
-# creating product dropdown selector
-default_products = ['FLOORS OR FLOORING MATERIALS', 
-                    'BASKETBALL, ACTIVITY AND RELATED EQUIPMENT', 
-                    'SOFAS, COUCHES, DAVENPORTS, DIVANS OR STUDIO COUCHES']
+neiss_products['Product'] = neiss_products['Product_1'].str.replace(r'[^\w\s]', '', regex=True).str.split().str[0]
 
-products = st.multiselect("Products", options=list(neiss_products["Product_1"].unique()), default=default_products)
-subset = neiss_products[neiss_products["Product_1"].isin(products)]
+# creating product dropdown selector
+default_products = ['FLOORS', 
+                    'BASKETBALL', 
+                    'SOFAS']
+
+products = st.multiselect("Products", options=list(neiss_products["Product"].unique()), default=default_products)
+subset = neiss_products[neiss_products["Product"].isin(products)]
 
 # labels for legend
 label = "if(datum.value == 1, '1: Treated, Released', " \
@@ -32,12 +34,12 @@ label = "if(datum.value == 1, '1: Treated, Released', " \
 
 # stacked bar chart of injuries
 chart = alt.Chart(subset).mark_bar().encode(
-    x=alt.X("Product_1:O", title="", axis=alt.Axis(labelAngle=90, labelLimit=500)),
+    x=alt.X("Product:O", title="", axis=alt.Axis(labelAngle=90, labelLimit=500)),
     y=alt.Y("count():Q", title="Number of Injuries"),
     color=alt.Color('Disposition:O', title='Disposition', 
                     scale=alt.Scale(scheme='reds'), 
                     legend=alt.Legend(title="Disposition", labelExpr=label)),
-    tooltip=["Disposition", "Product_1",  "count():Q"]
+    tooltip=["Disposition", "Product",  "count():Q"]
 ).properties(
     title='Head Injury Severity by Product',
 )
