7fb88b6f331b0daa851c6166040ae95181a7b72a
ananyaprasad07 <ananya_prasad@hms.harvard.edu>
2024-10-15 23:02:59 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,22 @@ neiss = pd.read_csv('neiss_head_injuries.tsv', sep='\t')
 
 st.write("Head Injuries Across the United States")
 
-default_products = []
+neiss_products = neiss
+top_products = neiss_products['Product_1'].value_counts().nlargest(20).index
+neiss_products = neiss_products[neiss_products['Product_1'].isin(top_products)]
+
+
+default_products = ['FLOORS OR FLOORING MATERIALS ', 
+                    'BASKETBALL, ACTIVITY AND RELATED EQUIPMENT', 
+                    'SOFAS, COUCHES, DAVENPORTS, DIVANS OR STUDIO COUCHES']
+
 products = st.multiselect("Products", options=list(neiss["Product_1"].unique()), default=default_products)
-subset = neiss[neiss["Product_1"].isin(products)]
+subset = neiss[neiss_products["Product_1"].isin(products)]
 
 chart = alt.Chart(subset).mark_bar().encode(
     x=alt.X("Product_1:O", title="Product Type"),
     y=alt.Y("count():Q", title="Number of Injuries"),
-    color=alt.Color('Disposition:O', title='Disposition', legend=alt.Legend(title="Disposition")),
+    color=alt.Color('Disposition:O', title='Disposition', scale=alt.Scale(scheme='viridis'), legend=alt.Legend(title="Disposition")),
     tooltip=["Disposition", "Product_1"]
 ).properties(
     title='Head Injury Severity by Product',
