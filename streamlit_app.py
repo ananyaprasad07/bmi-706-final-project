66ba473bcb6ca7cdbd99ba0f943832b5f2d392c8
ananyaprasad07 <ananya_prasad@hms.harvard.edu>
2024-10-15 22:37:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,17 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
+neiss = pd.read_csv('neiss_head_injuries.tsv', sep='\t')
+
+st.write("Injury Severity by Product")
+
+default_products = []
+products = st.multiselect("Products", options=list(neiss["Product_1"].unique()), default=default_products)
+subset = neiss[neiss["Products"].isin(products)]
+
+chart_product = alt.Chart(subset).mark_bar().encode(
+    x=alt.X("Product_1", title="Product Type"),
+    y=alt.Y("sum()").sort('-x')
+)
+
+st.altair_chart(chart_product, use_container_width=True)
