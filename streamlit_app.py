11b6a5f8f763100acda01e82c84aacfb8fd0440b
ananyaprasad07 <ananya_prasad@hms.harvard.edu>
2024-10-16 21:50:26 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 
+st.write("# Characterizing head Injuries in NEISS")
+
 neiss = pd.read_csv('neiss_head_injuries.tsv', sep='\t')
 
 st.write("## Demographics of Head Injuries")
