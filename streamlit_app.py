79ff0177397b59f1ec4d0418c9d195184953458c
Nancy2333333 <lanxin_zhang@hms.harvard.edu>
2024-10-16 21:18:58 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -145,9 +145,10 @@ chart_p3 = alt.Chart(subset_p3).mark_rect().encode(
     x=alt.X("Month:N",sort=months),
     y=alt.Y("Location:N"),
     color=alt.Color("Disposition: Q", title="Severity Score of Injury", 
-                          scale=alt.Scale(domain=[1, 6]))
+                          scale=alt.Scale(domain=[1, 6]),
+                   legend=alt.Legend(title="Disposition", labelExpr=label))
 ,
-    tooltip=["Disposition_Label:N"],
+    tooltip=["Disposition"],
 ).properties(
     title="Locational Injury Pattern Across the Year",
 )
